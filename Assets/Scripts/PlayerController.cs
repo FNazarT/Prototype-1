32978b76383c313040feda90117eafe426741360
Francisco Nazar <francisco.nazartorres@gmail.com>
2021-11-03 15:46:43 -0300

    First Commit
    Project backup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject driverCamera;
+    public string playerID;
+    private float speed = 20f;
+    private float turnSpeed = 40f;
+    private float horizontalInput, verticalInput;
+
+    void Update()
+    {
+        //This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal" + playerID);
+        verticalInput = Input.GetAxis("Vertical" + playerID);
+
+        // Switch camera view if player presses the Z Key
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (mainCamera.activeInHierarchy)
+            {
+                mainCamera.SetActive(false);
+                driverCamera.SetActive(true);
+            }
+            else
+            {
+                mainCamera.SetActive(true);
+                driverCamera.SetActive(false);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //We move the vehicle forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        //We rotate the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
---
+++ Assets/Scripts/RotatePropellerX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePropellerX : MonoBehaviour
+{
+    private float rotationSpeed = 200.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
